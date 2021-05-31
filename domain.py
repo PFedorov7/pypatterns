8f9e15d8978fdc103c840a340c90767c0d5a06d9
Pavel Fedorov <pfedorov7721@gmail.com>
2021-05-31 16:07:44 +0300

    using named objects

--- domain.py
+++ domain.py
@@ -1,4 +1,5 @@
 # First cut of a domain model for batches (model.py)
+from collections import Set
 from dataclasses import dataclass
 from datetime import date
 from typing import Optional
@@ -13,12 +14,45 @@ class OrderLine:
 
 class Batch:
     def __init__(
-            self, ref: str, sku: str, qty: int, eta: Optional[date]
+        self, ref: str, sku: str, qty: int, eta: Optional[date]
     ):
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = qty
+        self._purchased_quantity = qty
+        self._allocations = set()  # type: Set[OrderLine]
 
     def allocate(self, line: OrderLine):
-        self.available_quantity -= line.qty
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+
+
+#«MORE TYPES FOR MORE TYPE HINTS»
+# from typing import NewType
+#
+# Quantity = NewType("Quantity", int)
+# Sku = NewType("Sku", str)
+# Reference = NewType("Reference", str)
+# ...
+#
+# class Batch:
+#     def __init__(self, ref: Reference, sku: Sku, qty: Quantity):
+#         self.sku = sku
+#         self.reference = ref
+#         self._purchased_quantity = qty
---
+++ named_objects.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import NamedTuple
+from collections import namedtuple
+
+
+@dataclass(frozen=True)
+class Name:
+    first_name: str
+    surname: str
+
+
+class Money(NamedTuple):
+    currency: str
+    value: int
+
+
+Line = namedtuple('Line', ['sku', 'qty'])
+
+
+def test_equality():
+    assert Money('gbp', 10) == Money('gbp', 10)
+    assert Name('Harry', 'Percival') != Name('Bob', 'Gregory')
+    assert Line('RED-CHAIR', 5) == Line('RED-CHAIR', 5)
+
+
+fiver = Money('gbp', 5)
+tenner = Money('gbp', 10)
+
+
+def can_add_money_values_for_the_same_currency():
+    assert fiver + fiver == tenner
